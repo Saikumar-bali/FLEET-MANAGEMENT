795fe82f8953f45ca02525969f5d270020abb146
Saikumar Bali <saikumarbali55@gmail.com>
2026-06-23 18:04:13 +0530

    chore: remove temporary DB cleanup scripts

--- backend/scripts/wake-db.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-p.$queryRaw`SELECT 1`.then(() => {
-  console.log('DB connected');
-  return p.$disconnect();
-}).catch(e => {
-  console.log('DB error:', e.message?.substring(0, 200));
-  process.exit(1);
-});
